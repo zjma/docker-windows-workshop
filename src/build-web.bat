aaa85a4317bafd4de27d63db65da98d5eb50b655
Jeff Ma <zhouma@microsoft.com>
2019-05-31 13:16:04 -0700

    checkpoint

--- src/build-web.bat
+++ src/build-web.bat
@@ -1,8 +1,6 @@
 REM remove the .NET Core 2.1 projects which aren;t used in this build
 REM otherwise the nuget restore fails because .NET 4.7.2 can't work with Core 2.1
 
-del .\SignUp.Api.ReferenceData\SignUp.Api.ReferenceData.csproj
-del .\SignUp.MessageHandlers.IndexProspect\SignUp.MessageHandlers.IndexProspect.csproj
-
+dotnet restore SignUp.sln
 nuget restore SignUp.sln
 msbuild SignUp.Web\SignUp.Web.csproj /p:OutputPath=c:\out /p:Configuration=Release
